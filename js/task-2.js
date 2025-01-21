ba1dc1e3b5f7aeb4222008c60fbec80a330abf88
Olena Ivashchenko <olenaivaschenko@ukr.net>
2025-01-21 12:19:06 +0200

    fixed-task-2

--- js/task-2.js
+++ js/task-2.js
@@ -2,15 +2,16 @@ function makeArray(firstArray, secondArray, maxLength) {
   let commonArray = firstArray.concat(secondArray);
 
   let resultArray = [];
-  for (let i = 0; i < maxLength; i += 1) {
-    if (commonArray.length <= maxLength) {
-      resultArray = commonArray;
-    } else {
-      resultArray = commonArray.slice(0, maxLength);
-    }
+
+  if (commonArray.length <= maxLength) {
+    resultArray = commonArray;
+  } else {
+    resultArray = commonArray.slice(0, maxLength);
   }
+
   return resultArray;
 }
+
 console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
 console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
